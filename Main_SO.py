33921daf29ec179a3e4f129e928fdad0b4f14732
axl13917 <brayan.maytorena279236@potros.itson.edu.mx>
2026-05-12 20:22:43 -0600

    INTERFAZ GRAFICA

--- Main_SO.py
+++ Main_SO.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import ttk, simpledialog, messagebox
+from tkinter import ttk, simpledialog
+from tkinter import messagebox
+messagebox
 import threading
 import time
 import sys
@@ -15,53 +17,47 @@ class Proceso:
 #Clase sistema operativo
 class SistemaOperativo:
     def __init__(self):
-        self.peocesos = {}
+        self.procesos = {}
         self.siguiente_pid = 1
-    
-    def crear_proceso (self, nombre, tiempo_rafaga):
-        proceso = Proceso (self.siguiente_pid, nombre, tiempo_rafaga)
-        self.procesos [self.siguiente_pid] = proceso
 
-        print (f"Proceso creado -> PID: {proceso.pid}, Nombre: {proceso.nombre}")
+    def crear_proceso(self, nombre, tiempo_rafaga):
+        proceso = Proceso(self.siguiente_pid, nombre, tiempo_rafaga)
+        self.procesos[self.siguiente_pid] = proceso
 
+        print(f"Proceso creado: PID={proceso.pid}, Nombre={proceso.nombre}")
+       
         self.siguiente_pid += 1
-
-    def eliminar_proceso (self, pid):
+    def eliminar_proceso(self, pid):
         if pid in self.procesos:
-            print(f"Proceso eliminado -> PID: {pid}")
+         print(f"Proceso eliminado -> PID:{pid}")
         else:
-            print("ERROR: PID no encontrado")
+              print(f"ERROR: PID no encontrado")
+              def ejecutar_unidad_tiempo(self):
+                  #Buscar el primer proceso no encontrado
+                  for proceso in self.procesos.values():
+                        if proceso.tiempo_restante > 0:
+                            
+                           proceso.estado = "Ejecutando"
+                           proceso.tiempo_restante -= 1
+                          
+                           print(f"Ejecutando PID{proceso.pid}" f"- Tiempo restante: {proceso.tiempo_restante}")
+                            #verificar si termino
+                        if proceso.tiempo_restante == 0:
+                                  proceso.estado = "Terminado"
+                                  print(f"Proceso {proceso.pid}")
+                                  break
+                        #REDIRECCION DEL TEXTO
+                        class RedireccionTexto:
+                            def __init__(self, widget, ventana):
+                               self.widget = widget
+                               self.ventana = ventana
 
-    def ejecutar_unidad_tiempo(self):
-        #Buscar el primer proceso no terminado
-        for proceso in self.procesos.values():
-
-            if proceso.tiempo_restante > 0:
-
-                proceso.estado = "Ejecutando"
-                proceso.tiempo_restante -= 1
-
-                print(f"Ejecutando PID {proceso.pid}" f"- Tiempo restante: {proceso.tiempo_restante}")
-
-                #verificar si terminó
-                if proceso.tiempo_restante == 0:
-                    proceso.estado = "Terminado"
-                    print(f"Proceso {proceso.pid} terminado")
-                break
-            
-# REDIRECCIÓN DEL TEXTO
-class RedireccionTexto:
-    def __init__(self, widget, ventana):
-        self.widget = widget
-        self.ventana = ventana
-        
-    def write(self, texto):
-        def agregar():
-            self.widget.config(state='normal') 
-            self.widget.insert(tk.END, texto)
-            self.widget.see(tk.END)
-            self.widget.config(state='disabled')               
-        self.ventana.after(0, agregar)
-    def flush(self):
-        pass    
-            
+                            def write(self, texto):
+                                def agregar():
+                                    self.widget.config(state='normal')
+                                    self.widget.insert(tk.END, texto)
+                                    self.widget.see(tk.END)
+                                    self.widget.config(state='disabled')
+                                self.ventana.after(0, agregar)
+                            def flush(self):
+                                pass
